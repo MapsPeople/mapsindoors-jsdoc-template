9bd4990e5c71b29ac69088478b92e50357d35769
Esben Nepper <en@mapspeople.com>
2025-03-17 12:48:51 +0100

    fix: remove console.log statement from newDoclet handler

--- internal.js
+++ internal.js
@@ -1,8 +1,6 @@
-
 exports.handlers = {
     newDoclet: function ({doclet, ...e}) {
         if(doclet && doclet.tags?.some(tag => tag.title?.toLowerCase() === 'internal')) {
-            console.log(env);
             doclet.access = 'package';
         }
     }
